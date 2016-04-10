dd6fd531bb1318927f627ecd8d8cf5e5f9c92a29
mattvenn <matt@mattvenn.net>
2016-04-10 20:27:00 +0200

    more testing - abandoned now

--- tests/bbb-gpio-python/test_reset.py
+++ tests/bbb-gpio-python/test_reset.py
@@ -3,11 +3,20 @@ import time
 
 pin = "P9_12"
 GPIO.setup(pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.wait_for_edge(pin, GPIO.RISING)
+print "started"
+
+def callback(channel):
+    print "event detected!"
+
+GPIO.add_event_detect(pin, GPIO.RISING, callback=callback)
+#GPIO.add_event_detect(pin, GPIO.RISING)
 
 try:
     while True:
         time.sleep(0.5)
-        print(GPIO.input(pin))
+#        if GPIO.event_detected(pin):
+#            print "event detected!"
 except KeyboardInterrupt as e:
     GPIO.cleanup()
 
