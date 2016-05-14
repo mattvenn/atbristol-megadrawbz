23ee17fbcbe3b3dc4e282e3b1a23269f15f592b6
matt venn <matt@mattvenn.net>
2016-05-14 17:11:58 +0200

    setup

--- tests/screwthread-uniformity/plot.py
+++ tests/screwthread-uniformity/plot.py
@@ -1,11 +1,12 @@
 import math
 import pickle
 import matplotlib.pyplot as plt
+from sys import argv
 
 mm_per_step = (math.pi * 8) / (16 * 200 * 10)  # 16 time multistep don't know why the additional 10
-mm_per_pulse = (math.pi * 13) / 1600
+mm_per_pulse = 300.0 / 12606 # measured from graph 120mm long experiment
 
-with open('data.pkl') as fh:
+with open(argv[1]) as fh:
     data = pickle.load(fh)
 
 enc = []
@@ -15,8 +16,14 @@ for e in data['enc']:
 
 for p in data['pos']:
     pos.append(p * mm_per_step)
-
+print data['pos'][-1]
+from scipy import stats
+print(stats.pearsonr(enc, pos))
+slope, intercept, r_value, p_value, std_err = stats.linregress(enc, pos)
+print(slope, intercept, r_value, p_value, std_err)
 plt.plot(enc, pos)
+#plt.plot(enc, enc*slope  + intercept)
+plt.plot(pos, pos)
 plt.xlabel('enc')
 plt.ylabel('pos')
 plt.grid(True)
