3c7c0710c33f9c1a6f073675bf42c62d3e7e17b2
matt venn <matt@mattvenn.net>
2016-06-20 10:58:49 +0200

    added lcd

--- tests/cap-touch-gondola/cap.ino
+++ tests/cap-touch-gondola/cap.ino
@@ -1,4 +1,7 @@
 #include <CapacitiveSensor.h>
+#include <SPI.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_PCD8544.h>
 
 /*
  * CapitiveSense Library Demo Sketch
@@ -10,22 +13,42 @@
 
 CapacitiveSensor   cs_4_2 = CapacitiveSensor(4,2);        // 10M resistor between pins 4 & 2, pin 2 is sensor pin, add a wire and or foil if desired
 
-#define led 13
+// Software SPI (slower updates, more flexible pin options):
+// pin 7 - Serial clock out (SCLK)
+// pin 6 - Serial data out (DIN)
+// pin 5 - Data/Command select (D/C)
+// pin 4 - LCD chip select (CS)
+// pin 3 - LCD reset (RST)
+
+  //Adafruit_PCD8544(int8_t SCLK, int8_t DIN, int8_t DC, int8_t CS, int8_t RST);
+Adafruit_PCD8544 display = Adafruit_PCD8544(13,11,8,10,9);
+
+#define led 5
 void setup()                    
 {
    pinMode(led,OUTPUT);
    Serial.begin(9600);
+  display.begin();
+  display.clearDisplay();   // clears the screen and buffer
+  display.setContrast(45);
 }
 
 void loop()                    
 {
-    long start = millis();
     long total1 =  cs_4_2.capacitiveSensor(30);
-    if( total1 > 50)
+    /*
+    if( total1 > 30)
         digitalWrite(led, true);
     else
         digitalWrite(led, false);
+     */
     //Serial.println(total1);                  // print sensor output 1
 
-    delay(10);                             // arbitrary delay to limit data to serial port 
+    delay(250);                             // arbitrary delay to limit data to serial port 
+  display.clearDisplay();   // clears the screen and buffer
+  display.setCursor(0,0);
+  display.setTextSize(2);
+  display.setTextColor(BLACK);
+  display.println(total1);
+  display.display();
 }
